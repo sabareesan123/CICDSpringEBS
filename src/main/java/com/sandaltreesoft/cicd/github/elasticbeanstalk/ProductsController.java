0ff6ac8f3c4716135684bdfa322f4e6a6f80ca07
sabareesanm <sabareesanm@gmail.com>
2024-10-03 14:42:38 +0530

    spring rest api with get request

---
+++ src/main/java/com/sandaltreesoft/cicd/github/elasticbeanstalk/ProductsController.java
@@ -0,0 +1,17 @@
+package com.sandaltreesoft.cicd.github.elasticbeanstalk;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+public class ProductsController {
+
+    @GetMapping
+    public String greet()
+    {
+        return "Hello from elastic bean stalk"
+    }
+}
---
+++ src/test/java/com/sandaltreesoft/cicd/github/elasticbeanstalk/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandaltreesoft.cicd.github.elasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
